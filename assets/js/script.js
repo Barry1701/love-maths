0ece4f8a1296e9efb632b098d02d23dd6d7847cf
Barry1701 <>
2023-10-08 08:58:52 +0000

    Add changes to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,9 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === 'multiply') {
         displayMultiplyQuestion(num1, num2);
-    }else {
+    } else if (gameType === 'subtract'){
+        displaySubtractQuestion(num1, num2);
+    } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknow game type: ${gameType}. Aborting!`;
     }
@@ -62,7 +64,9 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, 'addition'];
     } else if (operator === 'x') {
         return [operand1 * operand2, 'multiply'];
-    }else {
+    } else if (operator === '-'){
+        return [operand1 - operand2, 'subtract'];
+    } else {
         alert(`Unimplementet operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting`;
     }
@@ -93,7 +97,10 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '-';
 
 }
 
